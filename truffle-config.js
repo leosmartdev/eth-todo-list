1386baadb25ec5f5a056d449c08aee3b02dc437e
leosultanov <Ilvirsultanovtchub21@gmail.com>
2022-02-08 19:27:56 +0300

    4.complete tasks

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,3 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-// const MNEMONIC = 'helmet pizza tongue blur apology error butter frost census consider shift someone';
-const MNEMONIC = 'olive inherit credit mansion will nuclear near sentence spare horse season twelve';
-
 module.exports = {
   networks: {
     development: {
@@ -9,13 +5,6 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     }
-  },
-  ropsten: {
-    provider: function() {
-      return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161")
-    },
-    network_id: 3,
-    gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
   },
   solc: {
     optimizer: {
